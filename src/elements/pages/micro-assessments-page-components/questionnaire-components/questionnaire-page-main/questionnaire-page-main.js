4af94aa6785b4265411658d64508dc390f5858bc
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-04-29 17:17:10 +0300

    Add risk status checking methods, add tests

--- src/elements/pages/micro-assessments-page-components/questionnaire-components/questionnaire-page-main/questionnaire-page-main.js
+++ src/elements/pages/micro-assessments-page-components/questionnaire-components/questionnaire-page-main/questionnaire-page-main.js
@@ -10,5 +10,30 @@ Polymer({
             },
             notify: true
         }
+    },
+    _checkCompleted: function(item) {
+        if (!item) { return false; }
+        let completed = true;
+
+        _.forEach(item.blueprints, blueprint => {
+            if (!blueprint.value && blueprint.value !== 0) {
+                completed = false;
+                return false;
+            }
+        });
+        if (!completed) { return false; }
+
+        _.forEach(item.children, child => {
+            if (!this._checkCompleted(child)) {
+                completed = false;
+                return false;
+            }
+        });
+        return completed;
+    },
+    _checkDisabled: function(index) {
+        if (!this.questionnaire.children || index === 0) { return false; }
+        let previous = this.questionnaire.children[index - 1];
+        return !this._checkCompleted(previous);
     }
 });
--- src/elements/pages/micro-assessments-page-components/questionnaire-components/risk-tab/risk-tab.js
+++ src/elements/pages/micro-assessments-page-components/questionnaire-components/risk-tab/risk-tab.js
@@ -14,6 +14,25 @@ Polymer({
         open: {
             type: Boolean,
             value: false
+        },
+        disabled: {
+            type: Boolean,
+            reflectToAttribute: true
+        },
+        completed: {
+            type: Boolean,
+            reflectToAttribute: true
         }
+    },
+    _setIndex: function(index) {
+        return index + 1;
+    },
+    showResults: function(completed, open) {
+        if (!completed) { return false; }
+        if (open) { return false; }
+        return true
+    },
+    getScore: function(score) {
+        return score || 0;
     }
 });
