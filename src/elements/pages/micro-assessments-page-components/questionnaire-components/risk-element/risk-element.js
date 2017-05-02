cda82c293c824b68dade28f71ada410cc82bdde0
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-05-02 13:36:01 +0300

    Add questionnaire content

---
+++ src/elements/pages/micro-assessments-page-components/questionnaire-components/risk-element/risk-element.js
@@ -0,0 +1,130 @@
+'use strict';
+
+Polymer({
+    is: 'risk-element',
+    properties: {
+        blueprint: {
+            type: Object,
+            value: function() {
+                return {};
+            },
+            notify: true
+        },
+        index: Number,
+        riskOptions: {
+            type: Object,
+            value: function() {
+                return [
+                    {label: 'N/A', value: 0},
+                    {label: 'Low', value: 1},
+                    {label: 'Medium', value: 2},
+                    {label: 'Significant', value: 3},
+                    {label: 'High', value: 4}
+                ];
+            }
+        },
+        extra: {
+            type: String,
+            value: '{"comments": "", "answer": "no"}',
+            observer: '_setValues'
+        },
+        answer_yes: {
+            type: Boolean,
+            value: false,
+            observer: 'setYes'
+        },
+        answer_no: {
+            type: Boolean,
+            value: false,
+            observer: 'setNo'
+        },
+        answer_na: {
+            type: Boolean,
+            value: false,
+            observer: 'setNa'
+        }
+    },
+
+    observers: [
+        // ,
+        // '_setValues(extra)'
+    ],
+
+    _setIndex: function(index) {
+        return index + 1;
+    },
+
+    _setRiskValue: function(value) {
+        if (!this.riskOptions || (!value && value !== 0)) { return value; }
+        if (typeof value !== 'object') {
+            return this.riskOptions[+value];
+        } else {
+            return value;
+        }
+    },
+
+    _setValues: function(values) {
+
+        if (_.isUndefined(values) || _.isNull(values)) { return; }
+
+        values = JSON.parse(values);
+
+        this.comments = values.comments || '';
+        if (values.answer) {
+            this[`answer_${values.answer}`] = true;
+        }
+    },
+
+    setYes: function(newV, oldV) {
+        if (newV) { this.setAnswer('yes'); }
+        if (oldV && !newV && this.checkRadio('yes')) { this.setAnswer('yes'); }
+    },
+    setNo: function(newV, oldV) {
+        if (newV) { this.setAnswer('no'); }
+        if (oldV && !newV && this.checkRadio('no')) { this.setAnswer('no'); }
+    },
+    setNa: function(newV, oldV) {
+        if (newV) { this.setAnswer('na'); }
+        if (oldV && !newV && this.checkRadio('na')) { this.setAnswer('na'); }
+    },
+
+    setAnswer: function(value) {
+        var answers = ['yes', 'no', 'na'];
+
+        answers.forEach(answer => {
+            this[`answer_${answer}`] = (answer === value);
+        });
+
+        this.setExtra();
+    },
+
+    checkRadio: function(value) {
+        var answers = ['yes', 'no', 'na'],
+            setToTrue = true;
+
+        answers.forEach(answer => {
+            if (answer !== value && this[`answer_${answer}`]) {
+                setToTrue = false;
+            }
+        });
+
+        return setToTrue;
+    },
+
+    _setRiskAnswer: function() {
+        if (this.answer_no) { return 'no'; }
+        if (this.answer_yes) { return 'yes'; }
+        return 'na';
+    },
+
+    setExtra: function() {
+        let comments = this.comments,
+            answer = this._setRiskAnswer();
+
+        this.blueprint.extra = JSON.stringify({comments: comments, answer: answer});
+    },
+
+    _setRiskAssessment: function(e, value) {
+        this.set('blueprint.value', value.selectedValues);
+    }
+});
--- src/elements/pages/micro-assessments-page-components/questionnaire-components/risk-tab/risk-tab.js
+++ src/elements/pages/micro-assessments-page-components/questionnaire-components/risk-tab/risk-tab.js
@@ -22,17 +22,52 @@ Polymer({
         completed: {
             type: Boolean,
             reflectToAttribute: true
+        },
+        riskOptions: {
+            type: Object,
+            value: function() {
+                return [
+                    {label: 'N/A', value: 0},
+                    {label: 'Low', value: 1},
+                    {label: 'Medium', value: 2},
+                    {label: 'Significant', value: 3},
+                    {label: 'High', value: 4}
+                ];
+            }
         }
     },
+    observers: [
+        'radioBtns(answerYes, answerNo, answerNa)'
+        // ,
+        // '_setValues()'
+    ],
+
     _setIndex: function(index) {
         return index + 1;
     },
+
     showResults: function(completed, open) {
         if (!completed) { return false; }
-        if (open) { return false; }
-        return true
+        return !open;
     },
+
     getScore: function(score) {
         return score || 0;
+    },
+
+    _setRiskValue: function(value) {
+        if (!this.riskOptions || (!value && value !== 0)) { return value; }
+        if (typeof value !== 'object') {
+            return this.riskOptions[+value];
+        } else {
+            return value;
+        }
+    },
+
+    radioBtns: function(answerYes, answerNo, answerNa) {
+        console.log(answerNa);
+        this.answerYes = answerYes ? true : false;
+        this.answerNo = answerNo ? true : false;
+        this.answerNa = answerNa ? true : false;
     }
 });
