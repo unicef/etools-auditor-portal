4e816730c55a2108594f0fa05d103970da41b05b
boslan <VuZ2!7W9>
2017-05-05 11:54:33 +0300

    Merge branch 'master' of https://bitbucket.org/razortheory/etools-ap
    # Conflicts:
    #	src/elements/pages/micro-assessments-page-components/micro-assessments-page-main/micro-assessments-page-main.html

--- src/elements/common-elements/engagement-report-components/assign-spot-check/assign-spot-check.js
+++ src/elements/common-elements/engagement-report-components/assign-spot-check/assign-spot-check.js
@@ -3,16 +3,34 @@
 Polymer({
     is: 'assign-spot-check',
     behaviors: [
-        APBehaviors.DateBehavior
+        APBehaviors.DateBehavior,
+        APBehaviors.PermissionController
     ],
     properties: {
-        disabledDateOfFieldVisit: Boolean,
-        disabledDateOfDraftReportToIp: Boolean,
-        disabledDateOfCommentsByIp: Boolean,
-        disabledDateOfDraftReportToUnicef: Boolean,
-        disabledDateOfCommentsByUnicef: Boolean
+        basePermissionPath: {
+            type: String,
+            observer: '_updateStyles'
+        }
     },
-    _isAvailable: function(prevDate, nextDate) {
-        return prevDate && !nextDate;
+    observers: [
+        '_updateStyles(data.date_of_field_visit)',
+        '_updateStyles(data.date_of_draft_report_to_ip)',
+        '_updateStyles(data.date_of_comments_by_ip)',
+        '_updateStyles(data.date_of_draft_report_to_unicef)',
+        '_updateStyles(data.date_of_comments_by_unicef)'
+    ],
+    _updateStyles: function() {
+        this.updateStyles();
+    },
+    _resetFieldError: function(event) {
+        event.target.invalid = false;
+    },
+    _isReadOnly: function(field, prevDate, nextDate) {
+        if (!this.basePermissionPath) { return true; }
+
+        let readOnly = this.isReadonly(`${this.basePermissionPath}.${field}`);
+        if (readOnly === null) { readOnly = true; }
+
+        return readOnly || !(prevDate && !nextDate);
     }
 });
