ba46f2d48f5f1a7fcb56ca3232ccc11675481bf6
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-06-13 12:36:35 +0300

    Add country change, try to fix status in edge

--- src/elements/core-elements/app-main-header/countries-dropdown/countries-dropdown.js
+++ src/elements/core-elements/app-main-header/countries-dropdown/countries-dropdown.js
@@ -2,6 +2,10 @@ Polymer({
 
     is: 'countries-dropdown',
 
+    behaviors: [
+        etoolsAppConfig.globals
+    ],
+
     properties: {
         opened: {
             type: Boolean,
@@ -21,6 +25,28 @@ Polymer({
 
     _countrySelected: function(e) {
         this.set('country', this.$.repeat.itemForElement(e.detail.item));
+    },
+
+    _changeCountry: function(event) {
+        let country = event && event.model && event.model.item,
+            id = country && country.id;
+
+        if (!id) { throw 'Can not find country id!'; }
+
+        this.fire('global-loading', {type: 'change-country', active: true, message: 'Please wait while country is changing...'});
+        this.countryData = {country: id};
+        this.url = this.getEndpoint('changeCountry').url;
+    },
+
+    _handleError: function() {
+        this.countryData = null;
+        this.url = null;
+        this.fire('global-loading', {type: 'change-country'});
+        this.fire('toast', {text: 'Can not change country. Please, try again later'});
+    },
+
+    _handleResponse: function() {
+        this.fire('main_refresh');
     }
 
 });
