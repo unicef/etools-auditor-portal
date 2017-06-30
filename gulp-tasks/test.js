b040a39246cb8b367f83647206ed9f46a48e386f
Anton Nikiforenko <nikiforenkoanton@gmail.com>
2017-06-30 18:48:52 +0300

    Fixed some tests. Run tests by small groups of files.

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -1,24 +1,58 @@
 const spawn = require('child_process').spawn;
 const argv = require('yargs').argv;
+const fs = require('fs');
 
 module.exports = function testElements(done) {
-    let tests = spawn('npm', ['test']);
+    let withErrors = false;
+    let files = fs.readdirSync('./build/tests/');
 
-    tests.stdout.on('data', (data) => {
-        if (~data.indexOf('Error')) data = `\x1b[31m${data}\x1b[0m`;
-        else if (~data.indexOf('ended with great success')) data = `\x1b[32m${data}\x1b[0m`;
-        console.log(`${data}`);
+    files = files.filter((src) => {
+        return src.search(/index\d.spec.html/) !== -1;
     });
 
-    tests.stderr.on('data', (data) => {
-        if (!argv.pc) {
-            console.log(`${data}`);
-        } else {
-            process.exit(1);
+    runTests(files);
+
+    function runTests() {
+        let indexFileName = files.shift();
+
+        if (!indexFileName) {
+            endedWithErrors(withErrors);
+            done();
+            return;
         }
-    });
 
-    tests.on('close', (code) => {
-        done();
-    });
+        let tests = spawn('npm', ['test', `./build/tests/${indexFileName}`]);
+
+        console.log(`\x1b[32mRunning ${indexFileName}.\x1b[0m`);
+
+        tests.stdout.on('data', (data) => {
+            if (~data.indexOf('✖') || ~data.indexOf('Tests failed:')) {
+                data = `\x1b[31m${data}\x1b[0m`;
+                withErrors = true;
+            } else if (~data.indexOf('ended with great success')) {
+                data = `\x1b[32m${data}\x1b[0m`;
+            }
+            console.log(`${data}`);
+        });
+
+        tests.stderr.on('data', (data) => {
+            console.log(`${data}`);
+            withErrors = true;
+            if (argv.pc) {
+                endedWithErrors(withErrors);
+                process.exit(1);
+            }
+        });
+
+        tests.on('close', (code) => {
+            runTests(files);
+            done();
+        });
+    }
+
+    function endedWithErrors(withErrors) {
+        if (withErrors) {
+            console.log(`\x1b[31mTests failed! See above for more details.\x1b[0m`);
+        }
+    }
 };
