63443a7be2ed0db855fe5db46acfd13d8f2fe537
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-07-07 12:26:58 +0300

    Refactor dialog-element, change delete logic for financial-findings-element

--- src/elements/pages/audits-page-components/report-page-components/financial-findings/financial-findings.js
+++ src/elements/pages/audits-page-components/report-page-components/financial-findings/financial-findings.js
@@ -52,12 +52,6 @@ Polymer({
                 'label': 'Amount USD',
                 'path': 'amount',
                 'align': 'right'
-            }, {
-                'size': '45px',
-                'label': 'Edit',
-                'name': 'edit',
-                'align': 'center',
-                'icon': true
             }]
         },
         details: {
@@ -103,7 +97,6 @@ Polymer({
 
     observers: [
         'resetDialog(dialogOpened)',
-        'changePermission(basePermissionPath)',
         '_updateFindings(dataItems)',
         '_errorHandler(errorObject.financial_finding_set)',
         '_checkNonField(errorObject.financial_finding_set)'
