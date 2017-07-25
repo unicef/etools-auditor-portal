3e629556882908c2e12ed63824b5b2c599595bab
Anton Nikiforenko <nikiforenkoanton@gmail.com>
2017-07-25 12:57:13 +0300

    Fixed filters. Changed filters tests.

--- src/elements/pages/engagements-page-components/engagements-list-view/engagements-list-view.js
+++ src/elements/pages/engagements-page-components/engagements-list-view/engagements-list-view.js
@@ -17,11 +17,121 @@
             },
             listHeadings: {
                 type: Array,
-                value: []
+                value: [{
+                    'size': 15,
+                    'label': 'Unique ID #',
+                    'name': 'unique_id',
+                    'link': '*engagement_type*/*data_id*/overview',
+                    'ordered': false,
+                    'path': 'unique_id'
+                }, {
+                    'size': 20,
+                    'label': 'Auditor',
+                    'name': 'agreement__auditor_firm__name',
+                    'ordered': false,
+                    'path': 'agreement.auditor_firm.name'
+                }, {
+                    'size': 20,
+                    'label': 'Partner Name',
+                    'name': 'partner__name',
+                    'ordered': false,
+                    'path': 'partner.name'
+                }, {
+                    'size': 15,
+                    'label': 'Engagement Type',
+                    'name': 'type',
+                    'ordered': false
+                }, {
+                    'size': 30,
+                    'label': 'Status',
+                    'name': 'status',
+                    'ordered': false,
+                    'additional': {
+                        'type': 'date',
+                        'path': 'status_date'
+                    }
+                }]
             },
             filters: {
                 type: Array,
-                value: []
+                value: [
+                    {
+                        name: 'auditor',
+                        query: 'agreement__auditor_firm',
+                        optionValue: 'id',
+                        optionLabel: 'name',
+                        selection: []
+                    },
+                    {
+                        name: 'engagement type',
+                        query: 'type',
+                        hideSearch: true,
+                        optionValue: 'value',
+                        optionLabel: 'label',
+                        selection: [{
+                            label: 'Audit',
+                            value: 'audit'
+                        }, {
+                            label: 'Micro Assessment',
+                            value: 'ma'
+                        }, {
+                            label: 'Spot Check',
+                            value: 'sc'
+                        }]
+                    },
+                    {
+                        name: 'partner',
+                        query: 'partner',
+                        optionValue: 'id',
+                        optionLabel: 'name',
+                        selection: []
+                    },
+                    {
+                        name: 'status',
+                        query: 'status',
+                        hideSearch: true,
+                        optionValue: 'value',
+                        optionLabel: 'label',
+                        selection: [
+                            {
+                                label: 'Partner was Contacted',
+                                value: 'partner_contacted'
+                            },
+                            {
+                                label: 'Field Visit',
+                                value: 'field_visit'
+                            },
+                            {
+                                label: 'Draft Report Issued To IP',
+                                value: 'draft_issued_to_unicef'
+                            },
+                            {
+                                label: 'Comments Received By IP',
+                                value: 'comments_received_by_partner'
+                            },
+                            {
+                                label: 'Draft Report Issued To UNICEF',
+                                value: 'draft_issued_to_partner'
+                            },
+                            {
+                                label: 'Comments Received By UNICEF',
+                                value: 'comments_received_by_unicef'
+                            },
+                            {
+                                label: 'Report Submitted',
+                                value: 'report_submitted'
+                            },
+                            {
+                                label: 'Final Report',
+                                value: 'final'
+                            },
+                            {
+                                label: 'Canceled',
+                                value: 'canceled'
+                            }
+                        ]
+                    }
+                ]
             },
             engagementsList: {
                 type: Array,
@@ -47,142 +157,26 @@
         },
 
         setupFiltersAndHeadings: function() {
-            let filters = [
-                {
-                    name: 'auditor',
-                    query: 'agreement__auditor_firm',
-                    optionValue: 'id',
-                    optionLabel: 'name',
-                    selection: []
-                },
-                {
-                    name: 'engagement type',
-                    query: 'type',
-                    hideSearch: true,
-                    optionValue: 'value',
-                    optionLabel: 'label',
-                    selection: [{
-                        label: 'Audit',
-                        value: 'audit'
-                    }, {
-                        label: 'Micro Assessment',
-                        value: 'ma'
-                    }, {
-                        label: 'Spot Check',
-                        value: 'sc'
-                    }]
-                },
-                {
-                    name: 'partner',
-                    query: 'partner',
-                    optionValue: 'id',
-                    optionLabel: 'name',
-                    selection: []
-                },
-                {
-                    name: 'status',
-                    query: 'status',
-                    hideSearch: true,
-                    optionValue: 'value',
-                    optionLabel: 'label',
-                    selection: [
-                        {
-                            label: 'Partner was Contacted',
-                            value: 'partner_contacted'
-                        },
-                        {
-                            label: 'Field Visit',
-                            value: 'field_visit'
-                        },
-                        {
-                            label: 'Draft Report Issued To IP',
-                            value: 'draft_issued_to_unicef'
-                        },
-                        {
-                            label: 'Comments Received By IP',
-                            value: 'comments_received_by_partner'
-                        },
-                        {
-                            label: 'Draft Report Issued To UNICEF',
-                            value: 'draft_issued_to_partner'
-                        },
-                        {
-                            label: 'Comments Received By UNICEF',
-                            value: 'comments_received_by_unicef'
-                        },
-                        {
-                            label: 'Report Submitted',
-                            value: 'report_submitted'
-                        },
-                        {
-                            label: 'Final Report',
-                            value: 'final'
-                        },
-                        {
-                            label: 'Canceled',
-                            value: 'canceled'
-                        }
-                    ]
-                }
-            ];
-
-            let listHeadings = [{
-                'size': 15,
-                'label': 'Unique ID #',
-                'name': 'unique_id',
-                'link': '*engagement_type*/*data_id*/overview',
-                'ordered': false,
-                'path': 'unique_id'
-            }, {
-                'size': 20,
-                'label': 'Auditor',
-                'name': 'agreement__auditor_firm__name',
-                'ordered': false,
-                'path': 'agreement.auditor_firm.name'
-            }, {
-                'size': 20,
-                'label': 'Partner Name',
-                'name': 'partner__name',
-                'ordered': false,
-                'path': 'partner.name'
-            }, {
-                'size': 15,
-                'label': 'Engagement Type',
-                'name': 'type',
-                'ordered': false
-            }, {
-                'size': 30,
-                'label': 'Status',
-                'name': 'status',
-                'ordered': false,
-                'additional': {
-                    'type': 'date',
-                    'path': 'status_date'
-                }
-            }];
-
             if (false) { //TODO: check that user is auditor
                 let auditorsFilterIndex = this._getFilterIndex('agreement__auditor_firm');
-                let auditorHeadingIndex = listHeadings.findIndex((heading) => {
+                let auditorHeadingIndex = this.listHeadings.findIndex((heading) => {
                     return heading.name === 'agreement__auditor_firm__name';
                 });
 
-                filters.splice(auditorsFilterIndex, 1);
-                listHeadings.splice(auditorHeadingIndex, 1);
-                listHeadings.forEach((heading) => {
+                this.filters.splice(auditorsFilterIndex, 1);
+                this.listHeadings.splice(auditorHeadingIndex, 1);
+                this.listHeadings.forEach((heading) => {
                     heading.size += 5;
                 });
             }
 
-            //TODO: remove let and if below
             let partners = this.getData('partners');
             if (!partners || !partners.length) {
                 let partnersFilterIndex = this._getFilterIndex('partner');
-                filters.splice(partnersFilterIndex, 1);
+                if (partnersFilterIndex !== -1) {
+                    this.filters.splice(partnersFilterIndex, 1);
+                }
             }
-
-            this.set('listHeadings', listHeadings);
-            this.set('filters', filters);
         },
 
         _getFilterIndex: function(query) {
