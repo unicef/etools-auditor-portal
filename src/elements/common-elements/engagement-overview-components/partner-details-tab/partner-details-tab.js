4557098cc72d7164054e8072e4ced99c2e35b2e5
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-07-25 09:40:03 +0300

    FIx partner officer error

--- src/elements/common-elements/engagement-overview-components/partner-details-tab/partner-details-tab.js
+++ src/elements/common-elements/engagement-overview-components/partner-details-tab/partner-details-tab.js
@@ -52,11 +52,11 @@ Polymer({
             this.partner.partnerOfficers = [engagementOfficer];
             this.authorizedOfficer = engagementOfficer;
         } else if (partner.partnerOfficers && partner.partnerOfficers.length) {
-            let officerIndex = ~_.findIndex(partner.partnerOfficers, officer => {
+            let officerIndex = !!(engagementOfficer && ~_.findIndex(partner.partnerOfficers, officer => {
                 return officer.id === engagementOfficer.id;
-            });
+            }));
 
-            this.authorizedOfficer = !!(engagementOfficer && officerIndex) ? engagementOfficer : partnerOfficer;
+            this.authorizedOfficer = officerIndex ? engagementOfficer : partnerOfficer;
         }
     },
 
