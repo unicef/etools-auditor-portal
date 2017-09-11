8bb78d8c1b47abde90fdf207ad8722f7148d3f46
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2017-09-11 14:49:47 +0300

    Refactor allowed actions logic

--- src/elements/common-elements/status-tab-element/action-buttons/action-buttons.js
+++ src/elements/common-elements/status-tab-element/action-buttons/action-buttons.js
@@ -29,11 +29,13 @@ Polymer({
         this.statusBtnMenuOpened = false;
     },
 
-    _setButtonText: function(text) {
-        if (!text) { return ''; }
-        if (!_.isString(text)) { throw 'Button text must be a string!'; }
+    _setButtonText: function(item) {
+        if (!item) { return ''; }
+        let text = item.display_name || item.replace('_', ' ');
 
-        return text.replace('_', ' ').toUpperCase();
+        if (!text) { throw 'Can not get button text!'; }
+
+        return text.toUpperCase();
     },
 
     _btnClicked: function(event) {
@@ -41,21 +43,24 @@ Polymer({
         let target = event.target.classList.contains('other-options') ? event.target : event.target.parentElement || event.target,
             isMainAction = !target.classList.contains('other-options') && !target.classList.contains('option-button') ;
 
-        let action = isMainAction ? this.actions[0] : target && target.getAttribute('event-name');
-        if (action) { this.fire(`action-activated`, {type: action}); }
-    },
+        let action = isMainAction ?
+            (this.actions[0].code || this.actions[0]) :
+            target && target.getAttribute('action-code');
 
-    _setBtnClass: function(length) {
-        return this._showOtherActions(length) ? 'with-actions' : '';
+        if (action) { this.fire(`action-activated`, {type: action}); }
     },
 
     _showOtherActions: function(length) {
         return length > 1;
     },
 
-    _setIcon: function(icon, icons) {
-        if (!icons || !icons) { return ''; }
-        return icons[icon] || '';
+    _setIcon: function(item, icons) {
+        if (!icons || !item) { return ''; }
+        return icons[(item.code || item)] || '';
+    },
+
+    _setActionCode: function(item) {
+        return item && (item.code || item);
     }
 
 });
