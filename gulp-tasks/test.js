802b7a89da6fa217b60512af74f23cc83e43c416
Uladzimir Urbanovich <uladzimir.u@razortheory.com>
2018-08-02 15:57:21 +0300

    improve tests output

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -23,27 +23,12 @@ module.exports = function testElements(done) {
 
         let tests;
         if (argv.noxvfb) {
-            tests = spawn('node', ['./node_modules/.bin/wct', '--skip-selenium-install', `./build/tests/${indexFileName}`]);
+            tests = spawn('node', ['./node_modules/.bin/wct', '--skip-selenium-install', `./build/tests/${indexFileName}`], { stdio: 'inherit' });
         } else {
-            tests = spawn('xvfb-run', ['-a', './node_modules/.bin/wct', '--skip-selenium-install', `./build/tests/${indexFileName}`]);
+            tests = spawn('xvfb-run', ['-a', './node_modules/.bin/wct', '--skip-selenium-install', `./build/tests/${indexFileName}`], { stdio: 'inherit' });
         }
 
         console.log(`\x1b[32mRunning ${indexFileName}.\x1b[0m`);
-
-        tests.stdout.on('data', (data) => {
-            if (~data.indexOf('✖') || ~data.indexOf('Tests failed')) {
-                data = `\x1b[31m${data}\x1b[0m`;
-                withErrors = true;
-            } else if (~data.indexOf('ended with great success') || data.indexOf('404') === 0) {
-                return;
-            }
-            console.log(`${data}`);
-        });
-
-        tests.stderr.on('data', (data) => {
-            console.log(`\x1b[31m${data}\x1b[0m`);
-            withErrors = true;
-        });
 
         tests.on('close', () => {
             runTests(files);
