23f6efef7a86e92d2338c91a8a8c971ec765e2c9
Bogdan Poslovsky <bogdan.p@razortheory.com>
2018-09-14 17:43:18 +0300

    Fix endpoint export for staff-sc

--- src/elements/pages/engagements-page-components/engagements-list-view/engagements-list-view.js
+++ src/elements/pages/engagements-page-components/engagements-list-view/engagements-list-view.js
@@ -125,7 +125,8 @@
             isStaffSc: {
                 type: Boolean,
                 value: false
-            }
+            },
+            endpointName: String
         },
 
         observers: ['_changeLinkTemplate(isStaffSc, listHeadings)'],
@@ -183,10 +184,12 @@
         },
 
         _setExportLinks: function() {
-            return [{
-                name: 'Export Engagements',
-                url: `${this.getEndpoint('engagementsList').url}csv/`
-            }];
+            const endpoint = this.getEndpoint(this.endpointName);
+            return endpoint ? [{
+                    name: 'Export Engagements',
+                    url: `${endpoint.url}csv/`
+                }] :
+                [];
         },
 
         _changeLinkTemplate: function(isStaffSc, headings) {
