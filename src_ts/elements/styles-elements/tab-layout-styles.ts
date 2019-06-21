b44042775beeefe38af22b9429c8e993ccc40fcb
Adrian Hangan <iovuadrian@gmail.com>
2019-06-21 16:25:54 +0300

    WIP styles migration

---
+++ src_ts/elements/styles-elements/tab-layout-styles.ts
@@ -0,0 +1,13 @@
+import {html} from '@polymer/polymer/polymer-element';
+
+
+// language=HTML
+export const tabLayoutStyles = html`
+<style>
+  :host {
+      position: relative;
+      display: block;
+      margin-bottom: 24px;
+  }
+</style>
+`;
