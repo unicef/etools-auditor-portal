53d817c7a1a9a14609c026ad0be19fd4b679dd56
danNordlogic <49279861+danNordlogic@users.noreply.github.com>
2019-07-17 14:10:32 +0300

    Migration to Poly3 filtering fix

--- src_ts/elements/common-elements/search-and-filter-element/search-and-filter.ts
+++ src_ts/elements/common-elements/search-and-filter-element/search-and-filter.ts
@@ -297,10 +297,10 @@ class SearchAndFilter extends QueryParamsController(PolymerElement) {
     let query = e.currentTarget.id;
     let queryObject = {page: '1'};
 
-    if (detail.selectedValues && query) {
+    if (detail.selectedItems && query) {
       let filter = this._getFilter(query);
       let optionValue = filter.optionValue || 'value';
-      queryObject[query] = detail.selectedValues.map(val => val[optionValue]).join(',');
+      queryObject[query] = detail.selectedItems.map(val => val[optionValue]).join(',');
     }
     this.updateQueries(queryObject);
 
