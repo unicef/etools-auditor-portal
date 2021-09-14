bab012fd7a150c3712b16d2086f3d1c51cd3e5ba
Robert Avram <robertavram@users.noreply.github.com>
2021-09-14 00:40:39 -0700

    Update config (#457)
    * update config and dockerfile
    
    * lower npm version for express to work
    
    Co-authored-by: Adriana Trif <trif.corina.adriana@gmail.com>

--- src_ts/elements/config/config.ts
+++ src_ts/elements/config/config.ts
@@ -29,28 +29,17 @@ window.EtoolsFamApp.DexieDb = etoolsCustomDexieDb;
 
 // -----------Environment------
 const PROD_DOMAIN = 'etools.unicef.org';
-const STAGING_DOMAIN = 'etools-staging.unicef.org';
-const DEV_DOMAIN = 'etools-dev.unicef.org';
-const DEMO_DOMAIN = 'etools-demo.unicef.org';
-const LOCAL_DOMAIN = 'localhost:8082';
+const STAGING_DOMAIN = 'etools-staging';
+const DEV_DOMAIN = 'etools-dev';
+const DEMO_DOMAIN = 'etools-demo';
+const LOCAL_DOMAIN = 'localhost';
 
 export const BASE_PATH = 'ap';
+export const BASE_URL = '/ap/';
 
 export const isProductionServer = () => {
   const location = window.location.href;
   return location.indexOf(PROD_DOMAIN) > -1;
-};
-
-export const isStagingServer = () => {
-  const location = window.location.href;
-  return location.indexOf(STAGING_DOMAIN) > -1;
-};
-
-export const isDevServer = () => {
-  return window.location.href.indexOf(DEV_DOMAIN) > -1;
-};
-export const isDemoServer = () => {
-  return window.location.href.indexOf(DEMO_DOMAIN) > -1;
 };
 
 export const checkEnvironment = () => {
@@ -77,20 +66,5 @@ export const resetOldUserData = () => {
 };
 
 export const getDomainByEnv = () => {
-  if (window.location.port === '8082') {
-    return `http://localhost:8082/${BASE_PATH}`;
-  }
-  if (isStagingServer()) {
-    return 'https://etools-staging.unicef.org/ap';
-  }
-  if (isDevServer()) {
-    return 'https://etools-dev.unicef.org/ap';
-  }
-  if (isDemoServer()) {
-    return 'https://etools-demo.unicef.org/ap';
-  }
-  if (isProductionServer()) {
-    return 'https://etools.unicef.org/ap';
-  }
-  return 'https://etools-dev.unicef.org/ap';
+  return window.location.protocol + window.location.host + BASE_URL.substr(0, BASE_URL.length - 1)
 };
