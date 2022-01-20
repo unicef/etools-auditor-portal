affa900679577e5e6111dad9a46d7c6e921660e0
Trif Adriana <trif.corina.adriana@gmail.com>
2022-01-20 18:47:06 +0200

    Fix readonly fields in Engagement Status

--- src_ts/elements/mixins/common-methods-mixin.ts
+++ src_ts/elements/mixins/common-methods-mixin.ts
@@ -233,7 +233,7 @@ function CommonMethodsMixin<T extends Constructor<PolymerElement>>(baseClass: T)
     dateHasChanged(e: CustomEvent) {
       const selDate = e.detail.date;
       // @ts-ignore
-      this.data[e.target.getAttribute('property-name')] = selDate;
+      this.set('data.' + e.target.getAttribute('property-name'), selDate);
     }
   }
 
