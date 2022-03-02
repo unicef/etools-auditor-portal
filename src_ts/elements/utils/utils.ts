59de56c6c2fd905b0f7cffadce0186bae0106747
Uladzimir Urbanovich <v_urbanovich@mail.ru>
2022-03-02 14:39:47 +0300

    [ch28340] Remove double bindings (#507)

---
+++ src_ts/elements/utils/utils.ts
@@ -0,0 +1,19 @@
+import {GenericObject} from '../../types/global';
+
+export function setProperty(object: GenericObject, path = '', dataToSet: any): any | null {
+  const pathArray = path.split('.');
+  let data = object;
+  while (pathArray.length) {
+    const field: string = pathArray.shift() as string;
+    if (pathArray.length) {
+      data = data[field] || {};
+    } else {
+      data[field] = dataToSet;
+    }
+  }
+}
+
+export function getProperty(object: GenericObject, path: string): any | null {
+  const pathArray = path.split('.');
+  return pathArray.reduce((data: GenericObject | null, field: string) => (data && data[field]) || null, object);
+}
