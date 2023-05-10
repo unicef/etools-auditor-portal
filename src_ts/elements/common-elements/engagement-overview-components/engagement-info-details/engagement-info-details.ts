1e1e1b02ddc919d8029fe4e4326d513fd2fb723e
Adriana Trif <trif.corina.adriana@gmail.com>
2023-05-10 13:51:29 +0300

    [ch33837]Use actual year instead of > 1 year ago (#608)

--- src_ts/elements/common-elements/engagement-overview-components/engagement-info-details/engagement-info-details.ts
+++ src_ts/elements/common-elements/engagement-overview-components/engagement-info-details/engagement-info-details.ts
@@ -700,7 +700,7 @@ class EngagementInfoDetails extends DateMixin(CommonMethodsMixin(PolymerElement)
       {label: String(currYear + 1), value: currYear + 1}
     ];
     if (savedYearOfAudit < currYear - 1) {
-      this.yearOfAuditOptions.unshift({value: savedYearOfAudit, label: '> 1 year ago'});
+      this.yearOfAuditOptions.unshift({value: savedYearOfAudit, label: String(savedYearOfAudit)});
     }
   }
 
