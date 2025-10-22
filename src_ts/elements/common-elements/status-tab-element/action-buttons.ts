827da2c966e83b5d884f2a809ba2e42a64ef9a9d
Dan <dan.cioara@nordlogic.com>
2025-10-22 16:43:33 +0300

    Lint fix

--- src_ts/elements/common-elements/status-tab-element/action-buttons.ts
+++ src_ts/elements/common-elements/status-tab-element/action-buttons.ts
@@ -9,8 +9,6 @@ import {moduleStyles} from '../../styles/module-styles';
 import {ActionButtonsStyles} from './action-buttons-styles';
 import {fireEvent} from '@unicef-polymer/etools-utils/dist/fire-event.util';
 import isEqual from 'lodash-es/isEqual';
-import {createDynamicDialog, removeDialog} from '@unicef-polymer/etools-unicef/src/etools-dialog/dynamic-dialog';
-import EtoolsDialog from '@unicef-polymer/etools-unicef/src/etools-dialog/etools-dialog';
 import {AnyObject} from '@unicef-polymer/etools-types';
 import '@unicef-polymer/etools-modules-common/dist/layout/are-you-sure';
 import {openDialog} from '@unicef-polymer/etools-utils/dist/dialog.util';
